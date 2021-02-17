0a7ea18692d6ebbe882a1a74f49cbbe0bcd0ab8e
Advya <50411324+Advya@users.noreply.github.com>
2021-02-17 17:03:46 +0200

    initial commit

--- app.rb
+++ app.rb
@@ -13,3 +13,36 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@option = params[:option]
+
+	@title = "Все готово!"
+	@message = "#{@username}, благодарим за запись, ждем вас в #{@datetime}, вы записались к #{@option}"
+	f = File.open './public/vizit_users.txt', 'a'
+	f.write "User: #{@username}, phone: #{@phone}, date: #{@datetime}, option #{@option}\n"
+	f.close
+
+	erb :message
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+	@email = params[:email]
+	@textmessage = params[:textmessage]
+
+	@title = "Все готово!"
+	@message = "Мы свяжемя с вами по этому емэйл: #{@email}"
+
+	z = File.open './public/contacts.txt', 'a'
+	z.write "Email: #{@email}, message: #{@textmessage}\n"
+	z.close
+
+	erb :message
+end
